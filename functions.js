df6e32fea4ddac4eef8cc13881551906ee9806d7
Moutie <108123837+Moutie12@users.noreply.github.com>
2022-07-29 08:51:23 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+
+function impliquer1() {
+    const back = document.getElementById('back11');
+    let ok1 = document.getElementById("ok1");
+    ok1.style.display="none";
+    back.classList.add("ajouter");
+}
+function impliquer2() {
+    const back = document.getElementById('back11');
+    let ok1 = document.getElementById("ok1");
+    ok1.style.display="block";
+    back.classList.remove("ajouter");
+}
